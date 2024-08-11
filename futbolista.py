0f8d9d3f4e8ae4c5345443e62aaa3f7fdfe25270
SebastiánMartínez <159493363+S3bastian2@users.noreply.github.com>
2024-08-11 17:28:50 -0500

    Correcciones de momento.

--- futbolista.py
+++ futbolista.py
@@ -2,6 +2,7 @@ from deportista import Deportista
 from persona import Persona
 
 class Futbolista(Persona, Deportista):
+    lista_futbolista = []
     def __init__(self, nombre, edad, altura, sexo, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
         Persona.__init__(self, nombre, edad, altura, sexo)
         Deportista.__init__(self, "Futbol", añosPracticando)
